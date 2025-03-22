65a8614ae254f1395080c2f28a118faefd2c3517
Darshan-Rajanna <darshanrajanna07@gmail.com>
2025-03-22 18:33:50 +0530

    Assignment of March 21

--- Assesment to create a flow of Classes in package/src/com/xworkz/governament/Minister.java
+++ Assesment to create a flow of Classes in package/src/com/xworkz/governament/Minister.java
@@ -9,11 +9,13 @@ public class Minister {
     private House house;
     private Security security;
     private State[] states;
+    private PersonnelAssistant personnelAssistant;
 
     public Minister(String name) {
         this.name = name;
         this.portfolios = new Portfolio[]{new Portfolio("Chief Minister")};
         this.house = new House();
+        this.personnelAssistant=new PersonnelAssistant("Siddharamiah");
         this.security = new Security();
         this.states = new State[]{new State("Karnataka")};
     }
@@ -24,6 +26,7 @@ public class Minister {
             portfolio.display();
         }
         house.display();
+        personnelAssistant.display();
         security.display();
         for (State state : states) {
             state.display();
